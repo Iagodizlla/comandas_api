f77c958805e67924666fbaa6971155d37518803e
iagodizlla <iagoschlemper@gmail.com>
2026-03-09 20:59:25 -0300

    Implementado integração de banco de dados inicial

--- src/domain/entities/Produto.py
+++
@@ -1,9 +0,0 @@
-# iago henrique schlemper
-from pydantic import BaseModel
-
-class Produto(BaseModel):
-    id_produto: int = None
-    nome: str
-    descricao: str = None
-    preco: float = 0.0
-    foto: bytes = None
